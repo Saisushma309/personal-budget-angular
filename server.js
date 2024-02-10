e562abec8d8749a206938e1bb60c21688030aa61
Neerajkumar Badam <nbadam@uncc.edu>
2024-02-10 10:07:01 -0500

    week 4 cors

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-// app.use(cors())
+app.use(cors())
 
 const budget = require('./data.json');
 
